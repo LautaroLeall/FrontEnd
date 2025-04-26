446bfbbad77989363476333075737e7347d7aa8d
LautaroLeall <lautaroleal4@gmail.com>
2025-04-26 02:17:17 -0300

    tp3 finzalido - js

--- 3erTP/scripts/1erP.js
+++ 3erTP/scripts/1erP.js
@@ -1,6 +1,11 @@
-const edad = prompt("Dime tu edad");
-if (edad >= 18) {
-    alert("Puedes conducir");
-} else {
-    alert("No puedes conducir");
+function validarEdad() {
+    const edad = prompt("Dime tu edad");
+    if (isNaN(edad)) {
+        alert("Ingresa un numero valido");
+        validarEdad();
+    } else if (edad >= 18) {
+        alert("Puedes conducir");
+    } else if (edad < 18) {
+        alert("No puedes conducir, debes tener 18 años o mas");
+    }
 }
