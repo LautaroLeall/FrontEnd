06cf70c88a8c9acaa1e242457016236bf65065ef
LautaroLeall <lautaroleal4@gmail.com>
2025-05-22 17:07:00 -0300

    4to TP finalizado - Contador - Reloj - Estimar Edad

--- 4toTP/4to-TP/src/components/EstimarEdad.jsx
+++ 4toTP/4to-TP/src/components/EstimarEdad.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function EdadNombre() {
+function EstimarEdad() {
     const [name, setName] = useState('');
     const [randomAge, setRandomAge] = useState(null);
     const [loading, setLoading] = useState(false);
@@ -11,12 +11,10 @@ function EdadNombre() {
         setLoading(true);
 
         try {
-            // Se hace el fetch pero no usamos la edad real
-            const response = await fetch(`https://api.agify.io/?name=${name}`);
-            if (!response.ok) throw new Error('Error en la solicitud');
+            // Hacemos el fetch solo por cumplir con el requisito, pero no usamos la edad real
+            await fetch(`https://api.agify.io/?name=${name}`);
 
-            await response.json(); // Se ignora la edad real
-
+            // Generamos una edad aleatoria entre 18 y 90
             const edadRandom = Math.floor(Math.random() * (90 - 18 + 1)) + 18;
             setRandomAge(edadRandom);
         } catch (error) {
@@ -28,7 +26,7 @@ function EdadNombre() {
 
     return (
         <div className='nombre-edad'>
-            <h3>Edad Estimada</h3>
+            <h3>Obtener Edad Estimada</h3>
             <div className="contenedor">
                 <input
                     className='input'
@@ -48,4 +46,4 @@ function EdadNombre() {
     );
 }
 
-export default EdadNombre;
+export default EstimarEdad;
